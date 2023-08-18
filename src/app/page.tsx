b66d62b05fd35899fd72ba0ba4a0a4c2abcbb813
ChawinNg <ik.chawin@gmail.com>
2023-08-18 16:40:10 +0700

    vaccard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import Banner from '@/components/banner'
+import VaccineCard from '@/components/vaccinecard'
 
 export default function Home() {
   return (
     <main>
       <Banner/>
+      <div style={{margin:"20px",display:"flex", flexDirection:"row",alignContent:"space-around",justifyContent:"space-around",flexWrap:"wrap"}}>
+        <VaccineCard/>
+      </div>
     </main>
   )
 }
---
+++ src/components/vaccinecard.tsx
@@ -0,0 +1,17 @@
+import styles from './vaccinecard.module.css'
+import Image from 'next/image';
+
+export default function VaccineCard(){
+    return (
+        <div className={styles.card}>
+            <div className={styles.cardimg}>
+                <Image src={'/img/covid.jpg'}
+                    alt='Covid Information'
+                    fill={true}
+                    objectFit='cover'/>
+            </div>
+            <div className={styles.cardtext}>Vaccines help develop immunity by imitating an infection. This type of infection induced by vaccines, however, almost never causes illness. In fact, it activates the immune system to produce antibodies and T-lymphocytes. Once the imitation infection induced by vaccines goes away, the body is left with a supply of memory T-lymphocytes, as well as B-lymphocytes that will remember how to fight off the disease in the future.</div>
+        </div>
+    );
+}
+
