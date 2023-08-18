9bfd8ec092f3d0205b16694a6a89a3fdeefa4230
ChawinNg <ik.chawin@gmail.com>
2023-08-18 17:05:09 +0700

    final

--- src/components/vaccinecard.tsx
+++ src/components/vaccinecard.tsx
@@ -10,7 +10,11 @@ export default function VaccineCard(){
                     fill={true}
                     objectFit='cover'/>
             </div>
-            <div className={styles.cardtext}>Vaccines help develop immunity by imitating an infection. This type of infection induced by vaccines, however, almost never causes illness. In fact, it activates the immune system to produce antibodies and T-lymphocytes. Once the imitation infection induced by vaccines goes away, the body is left with a supply of memory T-lymphocytes, as well as B-lymphocytes that will remember how to fight off the disease in the future.</div>
+            <div className={styles.cardtext}>Vaccines help develop immunity by imitating an infection. 
+            This type of infection induced by vaccines, however, almost never causes illness. 
+            In fact, it activates the immune system to produce antibodies and T-lymphocytes. 
+            Once the imitation infection induced by vaccines goes away, the body is left with a supply of memory T-lymphocytes, 
+            as well as B-lymphocytes that will remember how to fight off the disease in the future.</div>
         </div>
     );
 }
