ccb366566ff9dfe4be5590adc64441cb540dfc45
ChawinNg <ik.chawin@gmail.com>
2023-09-08 22:37:16 +0700

    Ass6_Up2

--- src/components/vaccinecard.tsx
+++ src/components/vaccinecard.tsx
@@ -8,14 +8,14 @@ export default function VaccineCard({name,imgsrc,delRating,onRating} : {name:str
     const [value,setValue] = React.useState<number | null>(2);
     return (
         <InteractiveCard>
-            <div className='w-full h-[50%] relative rounded-t-lg'>
+            <div className='w-full h-[70%] relative rounded-t-lg'>
                 <Image src={imgsrc}
                     alt='Covid Information'
                     fill={true}
                     className='object-cover rounded-t-lg'/>
             </div>
             <div className='w-full h-[15%] p-[10px] text-black font-bold text-center my-2 relative'>{name}</div>
-            <Rating className='p-auto my-2 object-contain mx-[115px] object-center content-center justify-center' defaultValue={4} value={value} onChange={(e,newValue)=> {setValue(newValue);e.stopPropagation(); onRating(name," : "+newValue)}}/>
+            <Rating className='mb-5 object-contain object-center' defaultValue={4} value={value} onChange={(e,newValue)=> {setValue(newValue);e.stopPropagation(); onRating(name," : "+newValue)}}/>
             {/* <button className="mx-auto flex text-sm h-[10%] rounded-md bg-sky-600 hover:bg-indigo-600 px-2 py-1 shadow-sm text-white"
             onClick={(e)=>{e.stopPropagation(); onRating(name)}}>Rating</button> */}
         </InteractiveCard>
