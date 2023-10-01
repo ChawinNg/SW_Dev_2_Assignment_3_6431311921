686c85fcf836b014f713c5e352387e12f531c177
ChawinNg <ik.chawin@gmail.com>
2023-10-01 14:45:00 +0700

    SwDev_Ass8_Finalll

--- src/components/PromoteCard.tsx
+++ src/components/PromoteCard.tsx
@@ -7,8 +7,13 @@ export function PromoteCard (){
     return (
         <div className='w-[80%] shadow-lg mx-[10%] my-10 p-2 rounded-lg bg-gray-200 flex flex-row'>
             <VideoPlayer isPlaying={playing} vdoSrc="/video/getvaccine.mp4"></VideoPlayer>
-            <div>
-                Get your Vaccine today.
+            <div className='m-5 relative'>
+            Get your Vaccine today.
+                <button className="block rounded-full bg-sky-600 hover:bg-indigo-600 px-9 py-2 mt-10
+                text-white shadow-sm bottom-0 absolute" onClick={()=> SetPlaying(!playing)}>
+                    {playing? 'Pause':'Play'}
+                </button>
+            
             </div>
         </div>
     )
--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -1,9 +1,21 @@
 'use client'
-
+import { useEffect } from "react"
 import { useRef } from "react"
+import { useWindowListener } from "@/hooks/useWindowListener"
+
 export function VideoPlayer({vdoSrc, isPlaying}: {vdoSrc:string,isPlaying:boolean}){
     const vdoRef = useRef<HTMLVideoElement>(null)
 
+    useEffect(()=>{
+        if(isPlaying){
+            vdoRef.current?.play()
+        } else {
+            vdoRef.current?.pause()
+        }
+    })
+
+    useWindowListener('contextmenu',(e)=>{e.preventDefault()})
+    
     return (
         <video className='w-[40%]' src={vdoSrc} ref={vdoRef} muted loop controls/>
     )
